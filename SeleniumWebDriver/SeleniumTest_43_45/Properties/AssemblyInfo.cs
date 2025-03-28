4ce97ea047b6e91110b02ddd69e334c99f8a1036
FongFus <npphus@gmail.com>
2025-03-28 22:26:29 +0700

    update web driver

---
+++ SeleniumWebDriver/SeleniumTest_43_45/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumTest_43_45")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumTest_43_45")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d06fa28d-989b-483b-bb4d-d8696d004496")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumWebDriver/SeleniumWebDriver/SearchProducts_45_Phu.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace SeleniumWebDriver
+{
+    partial class SearchProducts_45_Phu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_iP_45_Phu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_iP_45_Phu
+            // 
+            this.btn_iP_45_Phu.Location = new System.Drawing.Point(318, 142);
+            this.btn_iP_45_Phu.Name = "btn_iP_45_Phu";
+            this.btn_iP_45_Phu.Size = new System.Drawing.Size(117, 47);
+            this.btn_iP_45_Phu.TabIndex = 0;
+            this.btn_iP_45_Phu.Text = "iPhone 16 Pro Max 256GB";
+            this.btn_iP_45_Phu.UseVisualStyleBackColor = true;
+            this.btn_iP_45_Phu.Click += new System.EventHandler(this.btn_iP_45_Phu_Click);
+            // 
+            // SearchProducts_45_Phu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_iP_45_Phu);
+            this.Name = "SearchProducts_45_Phu";
+            this.Text = "SearchProducts_45_Phu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_iP_45_Phu;
+    }
+}
+
